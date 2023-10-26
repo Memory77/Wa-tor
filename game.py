0953f4edbc4fb6c73d2d338dbcb1d946875939b2
Farid Igouti <faridigouti@gmail.com>
2023-10-26 14:41:03 +0200

    makj

---
+++ game.py
@@ -0,0 +1,57 @@
+import random
+import pygame
+
+
+
+
+class Animaux:
+    def __init__(self, age_reproduction):
+        self.position = (0,0) #Position initial de l'animal
+        self.age_reproduction = age_reproduction
+         
+
+    
+    
+
+
+import Animaux 
+
+class Poisson(Animaux):
+    def __init__(self,age_reproduction):
+        self.age = age_reproduction
+    
+
+    
+    def reproduire(self, age_reproduction):
+        if self.age_reproduction >= chronon and case_vide():
+            nouveau_poisson = Poisson(0)
+            return nouveau_poisson
+        return None 
+    
+
+  
+#   dans class requin
+
+
+def manger_poisson(self, poisson):
+    if self.position == poisson.position:
+        # erngie + 1
+    
+
+
+
+    
+
+
+
+if nouveau_poisson:
+    # apparition écran à l'endroit vide 
+
+
+
+
+
+
+
+
+
