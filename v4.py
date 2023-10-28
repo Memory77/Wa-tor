34e46f13c4b86f5d50b842bc05ba1150b4a299a5
Deborah <soccio.deborah@gmail.com>
2023-10-28 15:01:28 +0200

    definition des methodes du poisson

--- v4.py
+++ v4.py
@@ -1,12 +1,16 @@
 import pygame
-import sys
 import random
+import time
 
 
-chronon = 0
 
-#definition de la classe poisson
-class Poisson(pygame.sprite.Sprite):
+#definition de la classe animaux
+class Animaux(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        
+#definition de la classe poisson qui herite de animaux
+class Poisson(Animaux):
     def __init__(self, x, y):
         super().__init__()
         self.image = pygame.image.load('img/poisson-rouge.png')
@@ -15,95 +19,166 @@ class Poisson(pygame.sprite.Sprite):
         self.rect.y = y
         self.temps_reproduction_poisson = 0  # age du poisson en chronons
 
-    def update(self):
-        self.temps_reproduction_poisson += 1
-        if self.temps_reproduction_poisson >= 30:
-            self.reproduire()
-
-    def reproduire(self):
-        # directions possibles pour la reproduction, ce sera jamais 0 0 car c'est le parent
+    def deplacer(self):
         directions = [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)]
-        random.shuffle(directions)  # Mélanger les directions pour choisir au hasard
+        random.shuffle(directions)
 
         for dx, dy in directions:
-            nouvelle_position_x = self.rect.x + dx * taille_case
-            nouvelle_position_y = self.rect.y + dy * taille_case
+            nouvelle_position_x = self.rect.x + dx * CASE_SIZE
+            nouvelle_position_y = self.rect.y + dy * CASE_SIZE
 
+            # gestion du tp
+            if nouvelle_position_x < 0:
+                nouvelle_position_x = WIDTH - CASE_SIZE
+            elif nouvelle_position_x >= WIDTH:
+                nouvelle_position_x = 0
+
+            if nouvelle_position_y < 0:
+                nouvelle_position_y = HEIGHT - CASE_SIZE
+            elif nouvelle_position_y >= HEIGHT:
+                nouvelle_position_y = 0
+
+            index_x = nouvelle_position_x // CASE_SIZE
+            index_y = nouvelle_position_y // CASE_SIZE
+
+            # verification si le point x et y remplissent les conditions pour le deplacement
+            if 0 <= index_x < WIDTH // CASE_SIZE and 0 <= index_y < HEIGHT // CASE_SIZE:
+                if not grille_poissons[index_y][index_x]:
+                    grille_poissons[self.rect.y // CASE_SIZE][self.rect.x // CASE_SIZE] = False
+                    grille_poissons[index_y][index_x] = True
+                    self.rect.x = nouvelle_position_x
+                    self.rect.y = nouvelle_position_y
+                    break
+
+    def update(self):
+        self.deplacer()
+        #stockage de la position pour faire des bébés
+        ancienne_position_x = self.rect.x  
+        ancienne_position_y = self.rect.y  
+        self.temps_reproduction_poisson += 1 #temps de reproduction en chronon (1 chronon = 0.5s voir la boucle de jeu)
+        if self.temps_reproduction_poisson >= 3:
+            self.reproduire(ancienne_position_x, ancienne_position_y)  
             
-            #on va verifier si :la nouvelle position x et y est entre 0 et la largeur, et que la case est libre grace au booleen
-            #il faudra changer par la suite faire en sorte que ça se delimite 
-            if (0 <= nouvelle_position_x < largeur and 0 <= nouvelle_position_y < hauteur and 
-                not grille_poissons[nouvelle_position_y // taille_case][nouvelle_position_x // taille_case]):
-                grille_poissons[nouvelle_position_y // taille_case][nouvelle_position_x // taille_case] = True
-                nouveau_poisson = Poisson(nouvelle_position_x, nouvelle_position_y)
-                tous_sprites.add(nouveau_poisson)
-                break  # Arrêter après avoir trouvé une position libre
+    def reproduire(self, ancienne_x, ancienne_y):
+        index_x = ancienne_x // CASE_SIZE
+        index_y = ancienne_y // CASE_SIZE
+        if not grille_poissons[index_y][index_x]:
+            grille_poissons[index_y][index_x] = True
+            nouveau_poisson = Poisson(ancienne_x, ancienne_y)
+            poissons.add(nouveau_poisson)
+            self.temps_reproduction_poisson = 0
+        
+        
+# class requin qui hérite de Poisson
+class Requin(Poisson):
+    def __init__(self, x, y):
+        super().__init__()
+        self.image = pygame.image.load('')
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+        self.temps_reproduction_poisson = 0
+        self.energie = 6
+        
+    def update(self):
+        super().__init__()
+        self.gestion_energie()
+        
+    def reproduction(self):
+        super().__init__()
 
-        self.temps_reproduction_poisson = 0  # Réinitialiser l'âge de reproduction
+    def gestion_energie(self):
+        pass
+    #if poisson and requin dans la même case 
+    #   poisson.kill())
+    #   self.energie += 1
+    #   compteur_sans_bouffe = 0 je reinitialise le compteur de la bouffe
+    # else 
+    #   self.energie -= 1
+    #   comteur_sans_bouffe += 1
+    #if compteur_sans_bouffe = 3
+    #   self.kill()
+    
+    # def mort(self, chronon_count, energie):
+    #    if energie == 0: #or (self.chronon_count >= 4 and compteur nourriture requin == 0):
+                            
+
+    # def nourriture(self, chronon_count, energie, poissons_manges):
+    #     if poisson and requin dans la même case
+    #         requin mange poisson (poisson.kill())
+    #         energie += 1
+    #     elif energie == 0 or (self.chronon_count >= 4 and compteur nourriture requin == 0):
+    #         self.kill()
+            
+
+
+
+
 
 # Initialisation de Pygame
 pygame.init()
 
-# taille fenêtre
-largeur, hauteur = 1500, 1000
-taille_case = largeur // 50
+
+WIDTH, HEIGHT = 1000, 1000
+CASE_SIZE = 100
 
 # creation fenetre
-fenetre = pygame.display.set_mode((largeur, hauteur))
-
-#definition de la grille
-#liste comprehension qui créé une grille 2d donc une liste de liste
-#largeur//taille_case calcule le nb de cases horizontales dans la grille et item hauteur
-grille_poissons = [[False] * (largeur // taille_case) for _ in range(hauteur // taille_case)]
-
+fenetre = pygame.display.set_mode((WIDTH, HEIGHT))
 
 # couleur du background
-ocean = (50, 50, 255)
+ocean = (255, 255, 255)
 
 #definition du background
 fenetre.fill(ocean)
 
 
-# Groupes de sprite
-tous_sprites = pygame.sprite.Group()
+#definition de la grille
+#liste comprehension qui créé une grille 2d donc une liste de liste
+grille_poissons = [[False] * (WIDTH // CASE_SIZE) for _ in range(HEIGHT // CASE_SIZE)]
 
-positions_poissons = []
 
-#nombre initial de poissons
-nombre_poissons = 5
+# dessin des lignes de la grille
+for x in range(0, WIDTH, CASE_SIZE): 
+    pygame.draw.line(fenetre, (0, 0, 0), (x, 0), (x, HEIGHT))
+    pygame.draw.line(fenetre, (0, 0, 0), (0, x), (WIDTH, x))
+# creation des groupes de sprite, un groupe pour les poissons et pour les requins(mais la classe pas encore faite)
+poissons = pygame.sprite.Group()
+requins = pygame.sprite.Group()#pour apres
 
-# Générez aléatoirement les positions de départ pour les poissons
-for _ in range(nombre_poissons):
-    x_case, y_case = random.randint(0, (largeur // taille_case) - 1), random.randint(0, (hauteur // taille_case) - 1)
-    while grille_poissons[y_case][x_case]:
-        x_case, y_case = random.randint(0, (largeur // taille_case) - 1), random.randint(0, (hauteur // taille_case) - 1)
-    positions_poissons.append((x_case, y_case))
+# exemple d'ajout pour un seul  poisson 
+poisson = Poisson(100, 100)
+poissons.add(poisson)
+poisson_2 = Poisson(300, 300)
+poissons.add(poisson_2)
 
-# Créez les poissons à ces positions
-for x_case, y_case in positions_poissons:
-    grille_poissons[y_case][x_case] = True
-    poisson = Poisson(x_case * taille_case, y_case * taille_case)
-    tous_sprites.add(poisson)
+### attention -> pour le moment le fait de rajouter les entités avec une boucle range ça fini par créé des chevauchements des poissons
+#je prefere pour le moment les initialiser comme plus haut
+# ajout pour plusieurs poisson avec la boucle
+# nb_poisson = 5
+
+# for poisson in range(nb_poisson):
+#     position_x = random.randint(0, WIDTH - CASE_SIZE) #randint entre 0 et width - case_size pour eviter qu'ils se retrouvent hors ecran
+#     position_y = random.randint(0, HEIGHT - CASE_SIZE)
+#     nouveau_poisson = (Poisson(position_x, position_y))
+#     poissons.add(nouveau_poisson)
     
-    
+
+chronon_count = 0 #chronon à 0 avant le lancement du jeu
 running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-    chronon += 1  # Incrémente le chronon à chaque itération de la boucle
-
-    #code juste pour voir les lignes de la grille definie plus haut
-    #for x in range(0, largeur, taille_case):
-    #    pygame.draw.line(fenetre, noir, (x, 0), (x, hauteur))
-    #for y in range(0, hauteur, taille_case):
-    #    pygame.draw.line(fenetre, noir, (0, y), (largeur, y))
-
-    tous_sprites.update()
-    tous_sprites.draw(fenetre)
-
+    
+    poissons.update()
+    poissons.draw(fenetre)
+    #definition d'un chronon à un demi seconde.
+    chronon = time.sleep(0.5) 
+    #compteur du chronon
+    chronon_count += 1
+    print(chronon_count) #voir la console
     pygame.display.update()
+
 
 # Quitter Pygame
 pygame.quit()
-sys.exit()
